1de7c5b01d8a19f41c1cec4a9d1e52f62073a83a
haffla <jakob.pupke@googlemail.com>
2015-09-20 21:18:23 +0200

    cleanup and pass session to main template

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -8,7 +8,7 @@ import scala.concurrent.Future
 
 class UserController extends Controller {
 
-  def list = Authenticated.async {
+  def list = Authenticated.async { implicit request =>
     val users:Future[Seq[User.Account#TableElementType]] = User.list
     users.map(
       res => Ok(views.html.users.list(res.toList))
