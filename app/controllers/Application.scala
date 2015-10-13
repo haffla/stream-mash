0925c9f9751447175931b22a71e98809aeee2775
haffla <jakob.pupke@googlemail.com>
2015-10-14 01:58:12 +0200

    add privacy view

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,14 @@
 package controllers
 
-import play.api.mvc.Controller
+import play.api.mvc.{Action, Controller}
 
 class Application extends Controller {
 
   def index = Authenticated { implicit request =>
     Ok(views.html.index())
+  }
+
+  def privacy = Action {
+    Ok(views.html.privacy())
   }
 }
