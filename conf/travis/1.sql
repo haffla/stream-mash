860e99d72bee186944e09ea30e36fc3cf85f7e38
haffla <jakob.pupke@googlemail.com>
2015-11-24 22:19:13 +0100

    add travis

--- conf/travis/1.sql
+++ conf/travis/1.sql
@@ -1,14 +1,9 @@
-# --- !Ups
-
 CREATE TABLE IF NOT EXISTS account(
     id_user SERIAL PRIMARY KEY,
     name VARCHAR(64) NOT NULL,
     password VARCHAR(64) NOT NULL,
     itunes_file_hash VARCHAR(32)
 );
-
-INSERT INTO account(name, password)
-VALUES('jacke', 'a92f6bdb75789bccc118adfcf704029aa58063c604bab4fcdd9cd126ef9b69af');
 
 CREATE TABLE IF NOT EXISTS album(
     id_album SERIAL,
@@ -20,19 +15,3 @@ CREATE TABLE IF NOT EXISTS album(
     UNIQUE (name,interpret,fk_user),
     UNIQUE (name,interpret,user_session_key)
 );
-
-CREATE TABLE IF NOT EXISTS artist(
-    id_artist SERIAL,
-    name VARCHAR(255) NOT NULL,
-    spotify_id VARCHAR(32) DEFAULT NULL,
-    rdio_id VARCHAR(32) DEFAULT NULL,
-    UNIQUE (name,spotify_id),
-    UNIQUE (name,rdio_id)
-);
-
-# --- !Downs
-
-DROP TABLE IF EXISTS album;
-DROP TABLE IF EXISTS account;
-DROP TABLE IF EXISTS artist;
-
