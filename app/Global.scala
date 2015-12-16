51d15b65db7161391b502d562fd2340937fbb2d5
haffla <jakob.pupke@googlemail.com>
2015-12-16 22:31:05 +0100

    add error page

--- app/Global.scala
+++ app/Global.scala
@@ -1,8 +1,33 @@
 import models.messaging.listen.ArtistIdListener
+import play.api.mvc.RequestHeader
+import play.api.mvc.Results._
+
+import scala.concurrent.Future
+
 
 object Global extends play.api.GlobalSettings {
 
   override def onStart(app: play.api.Application) {
     ArtistIdListener.listen()
+  }
+
+  override def onError(request: RequestHeader, ex: Throwable) = {
+    val title = "Melody Mess - No Luck"
+    val exceptionMessage = ex.getMessage
+    Future.successful(InternalServerError(views.html.error(
+      Map(
+        "exceptionMessage" -> exceptionMessage
+      )
+    )))
+  }
+
+  override def onHandlerNotFound(request: RequestHeader) = {
+    val path = request.path
+    Future.successful(NotFound(views.html.error(
+      Map(
+        "title" -> "Oups. The page you were looking for does not exist.",
+        "path" -> path
+      )
+    )))
   }
 }
