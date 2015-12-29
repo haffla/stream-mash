ae707a3516341913b4a7ad20809ce57ac12d80ec
Jakob Pupke <jakob.pupke@googlemail.com>
2015-12-29 13:20:11 +0100

    add uglify task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 var gulp = require('gulp');
 var webpack = require('webpack');
 var gutil = require('gutil');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
-gulp.task("build", function() {
-    webpack(require('./webpack.build.config'), function(err, stats) {
-        if(err) {
-            throw new gutil.PluginError("webpack", err);
+/* use webpack to compile all code into one file and put it in the public/javascripts folder */
+gulp.task("webpack", function() {
+    webpack(require('./webpack.build.config'), function(error, stats) {
+        if(error) {
+            throw new gutil.PluginError("webpack", error);
         }
-        gutil.log("[webpack]", stats.toString({}));
+        gutil.log("[webpack]", stats.toString());
     });
 });
+
+var JS_FOLDER = 'public/javascripts';
+
+gulp.task('uglify', ['webpack'], function() {
+  return gulp.src(JS_FOLDER + '/bundle.js')
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest(JS_FOLDER));
+});
+
+gulp.task('build', ['webpack', 'uglify']);
