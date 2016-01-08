dbca02bb4d343876d53d28d4d281edc23e0502bc
haffla <jakob.pupke@googlemail.com>
2016-01-08 21:11:39 +0100

    fix gutil still in code

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
 var gulp = require('gulp');
 var webpack = require('webpack');
-var gutil = require('gutil');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
 /* use webpack to compile all code into one file */
 gulp.task("webpack", function() {
-    webpack(require('./webpack.build.config'), function(error, stats) {
-        if(error) {
-            throw new gutil.PluginError("webpack", error);
-        }
-    });
+    webpack(require('./webpack.build.config'));
 });
 
 var JS_FOLDER = 'public/javascripts';
