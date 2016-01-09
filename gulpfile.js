6ec12898d39715332640a48dd0e4230353b42602
Jakob Pupke <jakob.pupke@googlemail.com>
2016-01-09 13:17:40 +0100

    bring back gutil. TODO make gulp task dependency work

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
 var gulp = require('gulp');
+var gutil = require('gutil');
 var webpack = require('webpack');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
 /* use webpack to compile all code into one file */
 gulp.task("webpack", function() {
-    webpack(require('./webpack.build.config'));
+    webpack(require('./webpack.build.config'), function(error, stats) {
+        if(error) {
+            throw new gutil.PluginError("webpack", error);
+        }
+        gutil.log("[webpack]", stats.toString());
+    });
 });
 
 var JS_FOLDER = 'public/javascripts';
 var DIST = 'react-frontend/dist';
 
 /* uglify javascript and output to public/javascripts folder */
-gulp.task('uglify', ['webpack'], function() {
+gulp.task('uglify', function() {
   return gulp.src('./' + DIST + '/bundle.js')
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest('./' + JS_FOLDER));
 });
-
-gulp.task('build', ['webpack', 'uglify']);
